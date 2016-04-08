c2f8339cf2bf943014093298d5817e0efb6d6dd6
XI449 <raggarwal@xebia.com>
2016-04-09 00:03:11 +0530

    goa destination

--- goa_destinations.js
+++ goa_destinations.js
@@ -21,81 +21,81 @@ var touristDestinations=
 	name:"Palolam Beach",
 	langitude:74.0232186,
 	lattitude:15.0099648,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
+	about:"Palolem Beach is largely unspoiled and is inhabited by both local fishermen and by foreign tourists who live in shacks along the shore or in the main village itself",
+	howToReachByAir:"Panaji, the capital, is more than 70 km away.",
+	howToReachByTrain:"Dabolim airport is about a 40-minutes drive away.",
 	howToReachByBus:""
 	},
 	{
 	name:"Mandrem Beach",
 	langitude:74.123996,
 	lattitude:15.2993265,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
-	howToReachByBus:""
+	about:"is a coastal village famous as a tourist spot in North Goa district of Goa state in India.[2] It is 21 km from the capital Panaji. It has two main beaches viz Junas and Ashvem.",
+	howToReachByAir:"Dabolim Airport is the closest Airport which is just about 33 km away from the Mandrem Beach.",
+	howToReachByTrain:"Mandrem does not have a train station, and the nearest station is Margao, at a distance of 57 kms from Mandrem",
+	howToReachByBus:"Goa is well-connected by road network to nearby foremost places and neighboring state. NH 17 links Goa to Mumbai."
 	},
 	{
 	name:"Cavelossim Beach",
 	langitude:73.941493,
 	lattitude:15.1715011,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
-	howToReachByBus:""
+	about:"The town hosts a well known beach at the southernmost tip of the Salcete beach stretch that starts from Majorda in the north to Cavelossim in the south.",
+	howToReachByAir:"Dabolim Airport is the closest Airport which is just about 33 km away from the Mandrem Beach.",
+	howToReachByTrain:"Mandrem does not have a train station, and the nearest station is Margao, at a distance of 57 kms from Mandrem",
+	howToReachByBus:"Goa is well-connected by road network to nearby foremost places and neighboring state. NH 17 links Goa to Mumbai."
 	},
 	{
 	name:"Benaulim Beach",
 	langitude:73.9187138,
 	lattitude:15.2570626,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
-	howToReachByBus:""
+	about:"is a coastal village famous as a tourist spot in North Goa district of Goa state in India.[2] It is 21 km from the capital Panaji. It has two main beaches viz Junas and Ashvem.",
+	howToReachByAir:"Dabolim Airport is the closest Airport which is just about 33 km away from the Mandrem Beach.",
+	howToReachByTrain:"Mandrem does not have a train station, and the nearest station is Margao, at a distance of 57 kms from Mandrem",
+	howToReachByBus:"Goa is well-connected by road network to nearby foremost places and neighboring state. NH 17 links Goa to Mumbai."
 	},
 	{
 	name:"Varca Beach",
 	langitude:73.9290015,
 	lattitude:15.2209348,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
-	howToReachByBus:""
+	about:"The town hosts a well known beach at the southernmost tip of the Salcete beach stretch that starts from Majorda in the north to Cavelossim in the south",
+	howToReachByAir:"Dabolim Airport is the closest Airport which is just about 33 km away from the Mandrem Beach.",
+	howToReachByTrain:"Mandrem does not have a train station, and the nearest station is Margao, at a distance of 57 kms from Mandrem",
+	howToReachByBus:"Goa is well-connected by road network to nearby foremost places and neighboring state. NH 17 links Goa to Mumbai."
 	},
 	{
 	name:"Basilica of Bom Jesus",
 	langitude:74.123996,
 	lattitude:15.2993265,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
+	about:" a coastal village famous as a tourist spot in North Goa district of Goa state in India.[2] It is 21 km from the capital Panaji. It has two main beaches viz Junas and Ashvem.",
+	howToReachByAir:"Dabolim airport is about a 90-minutes drive away.",
+	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away.",
 	howToReachByBus:""
 	},
 	{
 	name:"Arambol Beach",
 	langitude:73.7032836,
 	lattitude:15.6846886,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
-	howToReachByBus:""
+	about:"The town hosts a well known beach at the southernmost tip of the Salcete beach stretch that starts from Majorda in the north to Cavelossim in the south"",
+	hhowToReachByAir:"Dabolim Airport is the closest Airport which is just about 33 km away from the Mandrem Beach.",
+	howToReachByTrain:"Mandrem does not have a train station, and the nearest station is Margao, at a distance of 57 kms from Mandrem",
+	howToReachByBus:"Goa is well-connected by road network to nearby foremost places and neighboring state. NH 17 links Goa to Mumbai."
 	},
 	{
 	name:"Utorda Beach ",
 	langitude:73.8996066,
 	lattitude:15.3180882,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
+	about:"The town hosts a well known beach at the southernmost tip of the Salcete beach stretch that starts from Majorda in the north to Cavelossim in the south"",
+	howToReachByAir:"Dabolim airport is about a 90-minutes drive away.",
+	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away.",
 	howToReachByBus:""
 	},
 	{
 	name:"Dudhsagar Falls",
 	langitude:74.314067,
 	lattitude:15.3144691,
-	about:"",
-	howToReachByAir:"",
-	howToReachByTrain:"",
+	about:"The town hosts a well known beach at the southernmost tip of the Salcete beach stretch that starts from Majorda in the north to Cavelossim in the south"",
+	howToReachByAir:"Dabolim airport is about a 90-minutes drive away.",
+	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away.",
 	howToReachByBus:""
 	}
 ]
