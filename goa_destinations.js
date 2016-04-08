51a5f0e5ccd18fe6d6e37a504d82cd34fa3bd630
Pratima Pant <ppant@beachbody.com>
2016-04-08 23:24:27 +0530

    map

--- goa_destinations.js
+++ goa_destinations.js
@@ -1,58 +1,20 @@
-var destination=Goa
-var touristDestination[]=
-{
-	{
-	name:"Agonda Beach"
-	langitude:73.9857577
-	lattitude:15.0437626
-	about:"Agonda is a great spot for sunbathing, relaxing and swimming, with still relatively few tourists. Agonda is a nice long pristine stretch of beach. Agonda is generally cheaper, quieter, hassle-free and has much less "touristic" feel."
-	howToReachByAir:"Dabolim airport is about a 90-minutes drive away."
-	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away."
-	howToReachByBus:
-	},
-	{
-	name:"Palolam Beach"
-	langitude:74.0232186
-	lattitude:15.0099648
-	},
-	{
-	name:"Mandrem Beach"
-	langitude:74.123996
-	lattitude:15.2993265
-	},
-	{
-	name:"Cavelossim Beach"
-	langitude:73.941493
-	lattitude:15.1715011
-	},
-	{
-	name:"Benaulim Beach"
-	langitude:73.9187138
-	lattitude:15.2570626
-	},
-	{
-	name:"Varca Beach"
-	langitude:73.9290015
-	lattitude:15.2209348
-	},
-	{
-	name:"Basilica of Bom Jesus"
-	langitude:74.123996
-	lattitude:15.2993265
-	},
-	{
-	name:"Arambol Beach"
-	langitude:73.7032836
-	lattitude:15.6846886
-	},
-	{
-	name:"Utorda Beach "
-	langitude:73.8996066
-	lattitude:15.3180882
-	},
-	{
-	name:"Dudhsagar Falls"
-	langitude:74.314067
-	lattitude:15.3144691
-	}
+var destination={
+	name:"Goa",
+	pic:"goa.jpg",
+	longitude:73.9830029,
+	lattitude:15.4253792,
+	about:"Goa with its beaches, all-night parties and laid-back tropical vibe is a blissful surprise. You will love hanging around by the beach, cruising through impossibly green countryside on two wheels, and the evening ritual of watching the sun melt into the Arabian Sea."
 }
+var touristDestinations=
+[
+	{
+	name:"Agonda Beach",
+	pic:"agonda",
+	langitude:73.9857577,
+	lattitude:15.0437626,
+	about:"Agonda is a great spot for sunbathing, relaxing and swimming, with still relatively few tourists. Agonda is a nice long pristine stretch of beach. Agonda is generally cheaper, quieter, hassle-free and has much less 'touristic' feel.",
+	howToReachByAir:"Dabolim airport is about a 90-minutes drive away.",
+	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away.",
+	howToReachByBus:""
+	}
+]
