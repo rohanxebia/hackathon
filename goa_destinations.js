ad3ca8e945339b7fd0a832524a065a31ea50f4ef
Pratima Pant <ppant@beachbody.com>
2016-04-08 23:38:56 +0530

    changes

--- goa_destinations.js
+++ goa_destinations.js
@@ -9,12 +9,93 @@ var touristDestinations=
 [
 	{
 	name:"Agonda Beach",
-	pic:"agonda",
+	pic:"agonda.jpg",
 	langitude:73.9857577,
 	lattitude:15.0437626,
 	about:"Agonda is a great spot for sunbathing, relaxing and swimming, with still relatively few tourists. Agonda is a nice long pristine stretch of beach. Agonda is generally cheaper, quieter, hassle-free and has much less 'touristic' feel.",
 	howToReachByAir:"Dabolim airport is about a 90-minutes drive away.",
 	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away.",
+	howToReachByBus:""
+	},
+	{
+	name:"Palolam Beach",
+	langitude:74.0232186,
+	lattitude:15.0099648,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Mandrem Beach",
+	langitude:74.123996,
+	lattitude:15.2993265,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Cavelossim Beach",
+	langitude:73.941493,
+	lattitude:15.1715011,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Benaulim Beach",
+	langitude:73.9187138,
+	lattitude:15.2570626,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Varca Beach",
+	langitude:73.9290015,
+	lattitude:15.2209348,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Basilica of Bom Jesus",
+	langitude:74.123996,
+	lattitude:15.2993265,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Arambol Beach",
+	langitude:73.7032836,
+	lattitude:15.6846886,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Utorda Beach ",
+	langitude:73.8996066,
+	lattitude:15.3180882,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
+	howToReachByBus:""
+	},
+	{
+	name:"Dudhsagar Falls",
+	langitude:74.314067,
+	lattitude:15.3144691,
+	about:"",
+	howToReachByAir:"",
+	howToReachByTrain:"",
 	howToReachByBus:""
 	}
 ]
