955078a3e611d9592321a56f875df326a5f3178c
Pratima Pant <ppant@beachbody.com>
2016-04-09 00:58:17 +0530

    map integration

--- goa_destinations.js
+++ goa_destinations.js
@@ -4,14 +4,13 @@ var destination={
 	longitude:73.9830029,
 	lattitude:15.4253792,
 	about:"Goa with its beaches, all-night parties and laid-back tropical vibe is a blissful surprise. You will love hanging around by the beach, cruising through impossibly green countryside on two wheels, and the evening ritual of watching the sun melt into the Arabian Sea."
-}
+};
 var touristDestinations=
-[
-	{
+[{
 	name:"Agonda Beach",
 	pic:"agonda.jpg",
-	langitude:73.9857577,
-	lattitude:15.0437626,
+	langitude:77.2249600,//73.9857577,
+	lattitude:28.6353080, //15.0437626,
 	about:"Agonda is a great spot for sunbathing, relaxing and swimming, with still relatively few tourists. Agonda is a nice long pristine stretch of beach. Agonda is generally cheaper, quieter, hassle-free and has much less 'touristic' feel.",
 	howToReachByAir:"Dabolim airport is about a 90-minutes drive away.",
 	howToReachByTrain:"Agonda is only a few kilometres from Canacona station, although main train services stop at Margao station, about an hour’s drive away.",
